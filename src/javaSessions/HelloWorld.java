37ad343168c1aca644407d08b373d029d00ec1ea
Ramachandrareddy <42475784+ramachandrareddyk@users.noreply.github.com>
2023-05-15 09:13:33 +0530

    first commit

---
+++ src/javaSessions/HelloWorld.java
@@ -0,0 +1,13 @@
+package javaSessions;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello This is my First java Program");
+		
+		System.out.println("hello Testing");
+
+	}
+
+}
---
+++ src/javaSessions/Test.java
@@ -0,0 +1,17 @@
+package javaSessions;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+	
+	//Assignment
+	
+	//Create 3 java projects
+	//create 3 packages in each project//9
+	//create 3 classes in each package//27
+
+}
+
+
